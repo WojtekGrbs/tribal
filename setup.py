391bc0cef90b1039adb23989cefa566f642e5141
Wojtasauce <51636941+WojtekGrbs@users.noreply.github.com>
2025-01-29 00:04:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -114,6 +114,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
